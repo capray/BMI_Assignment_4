c967d6b6b59bdbf9b18e0e428aabb1632feaa373
capray <iiryul@gmail.com>
2017-08-10 13:48:04 -0400

    importing excel

--- kim_jason__test.py
+++ kim_jason__test.py
@@ -1,2 +1,13 @@
 # test file
-# !
+import pandas as pd;
+
+# Plate1 = pd.read_excel('06222016 Staph Array Data.xlsx',na_values='',sheetname="Plate 1");
+full_xls = pd.ExcelFile('06222016 Staph Array Data.xlsx');
+
+# will come back to grab all sheets, work with first plate
+# for sheet in range(11):
+#     pass;
+
+df_plate1 = full_xls.parse(0,skiprows=1);
+
+print(df_plate1)
